a0ad9683cc685a48c32efd2255d6ffdd77dc4486
ananthpriyan <125973589+ananthpriyan@users.noreply.github.com>
2023-06-26 18:05:06 +0530

    first anand

---
+++ src/com/practice/Git.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+public class Git {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Thia is master");
+		System.out.println("Thia is Anand");
+	}
+
+}
